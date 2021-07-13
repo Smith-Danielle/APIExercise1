7a2f658c92235caf7fd68dbcabd4c33f7d0f9076
Smith-Danielle <smith.ddms@gmail.com>
2021-07-13 17:45:08 -0500

    API Exercise 1

---
+++ APIExercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace APIExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteHouse quote = new QuoteHouse();
+            Console.WriteLine("Conversation between Kayne West and Ron Swanson");
+            Console.WriteLine("-----------------------------------------------");
+            for (int i = 0; i < 3; i++)
+            {
+                quote.Kayne();
+                Thread.Sleep(1400);
+                quote.Ron();
+                Thread.Sleep(1400);
+            }
+            
+        }
+    }
+}
---
+++ APIExercise1/QuoteHouse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace APIExercise1
+{
+    public class QuoteHouse
+    {
+        public QuoteHouse()
+        {
+        }
+
+        public void Kayne()
+        {
+            var client = new HttpClient();
+
+            string urlKanyeW = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(urlKanyeW).Result;
+
+            var kWQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kayne: {kWQuote}");
+        }
+
+        public void Ron()
+        {
+            var client = new HttpClient();
+
+            string urlRonS = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(urlRonS).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"Ron: {ronQuote}");
+        }             
+       
+    }
+}
